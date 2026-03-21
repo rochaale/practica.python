25280793636929a63f30a5d57778e0b92ad853ad
Alejandro Rocha <rocha_ale26@hotmail.com>
2026-03-21 18:31:34 -0300

    ejercicio 4

--- introduccion.py
+++ introduccion.py
@@ -23,3 +23,15 @@ numero = int(input("Ingrese un numero: "))
 for i in range(1,11):
     mult = i * numero
     print(f"{numero} x {i} = {mult}")
+    
+#Crea un programa que dado un número N ingresado por el usuario, imprima los
+#números del 1 al N pero saltee los múltiplos de 5. Nota: utilizá la sentencia continue
+#donde haga falta.
+
+numero = int(input("ingresa un nuemero"))
+for i in range(1,numero+1):
+    if i % 5 == 0:
+        continue
+    print(i)
+    
+
